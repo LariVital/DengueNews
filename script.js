baff9732e62e708c505c88647d0baa9708735610
Giovanna D'Alcântara <alcangio@protonmail.com>
2024-03-31 10:36:56 -0300

    inserida condicional em script.js para definir url distinta caso a página não seja a index, de modo a abrir a página newsletter como pop-up

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 // abre a página newsletter como pop-up
-function openPopup() {
-    window.open("./public/newsletter.html", "popup", "width=800,height=400");
+if (window.location.pathname === '/index.html') {
+    function openPopup() {
+        window.open("./public/newsletter.html", "popup", "width=800,height=400");
+    }
+} else {
+    function openPopup() {
+        window.open("./newsletter.html", "popup", "width=800,height=400");
+    }
 }
