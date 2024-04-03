74e9b9c226d4f88e6e6d4b9873c5696920116345
Giovanna D'Alcântara <alcangio@protonmail.com>
2024-04-03 17:41:56 -0300

    finalizadas seções chikungunya e zika de faq.html

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 // abre a página newsletter como pop-up
-if (window.location.pathname === '/index.html') {
-    function openPopup() {
-        window.open("./public/newsletter.html", "popup", "width=800,height=400");
-    }
-} else {
-    function openPopup() {
-        window.open("./newsletter.html", "popup", "width=800,height=400");
+function openPopup() {
+    if (window.location.pathname === '/index.html') {
+        window.open('./public/newsletter.html', 'popup', 'width=800,height=400');
+    } else if (window.location.pathname !== '/index.html') {
+        window.open('./newsletter.html', 'popup', 'width=800,height=400');
     }
 }
