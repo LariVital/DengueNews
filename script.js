ed8a0e1bc97383326f5574d63b7be053f8934792
Giovanna D'Alcântara <alcangio@protonmail.com>
2024-04-12 09:34:06 -0300

    atualizado cor gradiente em quem_somos

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 // NEWSLETTER
 // abre a página newsletter como pop-up
-// function openPopup() {
-//     if (window.location.pathname === '/index.html') {
-//         window.open('./public/newsletter.html', 'popup', 'width=800,height=400');
-//     } else if (window.location.pathname !== '/index.html') {
-//         window.open('./newsletter.html', 'popup', 'width=800,height=400');
-//     }
-// }
 
 let newsPopUp = document.getElementsByClassName("news-pop")[0];
 console.log(newsPopUp)
