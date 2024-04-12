fad20024c2b51530fd2838c87424b561ed500951
Giovanna D'Alcântara <alcangio@protonmail.com>
2024-04-12 08:54:13 -0300

    testando js no newsletter popup

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 // NEWSLETTER
 // abre a página newsletter como pop-up
-function openPopup() {
+// function openPopup() {
+//     if (window.location.pathname === '/index.html') {
+//         window.open('./public/newsletter.html', 'popup', 'width=800,height=400');
+//     } else if (window.location.pathname !== '/index.html') {
+//         window.open('./newsletter.html', 'popup', 'width=800,height=400');
+//     }
+// }
+
+let newsPopUp = document.getElementsByClassName("news-pop")[0];
+console.log(newsPopUp)
+
+newsPopUp.addEventListener("click", () => {
     if (window.location.pathname === '/index.html') {
         window.open('./public/newsletter.html', 'popup', 'width=800,height=400');
-    } else if (window.location.pathname !== '/index.html') {
+    } else {
         window.open('./newsletter.html', 'popup', 'width=800,height=400');
     }
-}
+})
 
 //Carousel página inicial
 var radio = document.querySelector('.manual-btn')
