f5b2bb68fe2ae45765619a81d62b121a6745e778
Caiki <caiki.luiz@gmail.com>
2024-05-12 18:58:51 -0300

    alterações

--- formulario.js
+++ formulario.js
@@ -1,65 +1,58 @@
-//Validação formulário Fale conosco 
-const formHome = document.getElementById('formHome');
-console.log(formHome);
-const nameInput = document.querySelector("#campoName");
-console.log(campoName); 
-const emailInput = document.querySelector("#email");
-console.log(email);
-const spanInput = document.querySelectorAll("spans");
-console.log(spans);
+//nome
+let usernameInput = document.getElementById("username");
+let usernameLabel = document.querySelector('label[for="username"]');
+let usernameSpans = document.getElementById("spans");
+
+//email
+let emailLabel = document.querySelector('label[for="email"]');
+let emailInput = document.getElementById("email");
+let emailSpans = document.getElementById("spans");
 
 
-formHome.addEventListener("submit", (event) => {
-    event.preventDefault();
-    //Verificar se o nome está vazio
-    if (nameInput.value === "") {
-        alert("Por favor, preencha o seu nome");
-        return;
-    }
-    //Verificar se o e-mail está preenchido e se é válido
-    if(emailInput.value === "" || !isEmailValid(emailInput.value) ){
-        alert("Por favor, preencha o seu email");
-        return;
-    }
-
-    //Se todos os campos estiverem corretamente preenchidos, envie o form
-    formHome.submit();
-});
-
-// Função que válida e-mail
-function isEmailValid(email){
-    const emailRegex = new RegExp(
-        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
-    );
-    if(emailRegex.test(email)) {
-        return true
-    }
-    return false;
+// Campo obrigatório para o User name
+usernameInput.addEventListener("focus", function(){
+    usernameLabel.classList.add("required-popup");
+})
+//----------Ocultar popup de campo obrigatório----------//
+usernameInput.addEventListener("blur", function(){
+    usernameLabel.classList.remove("required-popup");
+})
+//----------Validar valor do input Username----------//
+usernameInput.addEventListener("input", function(e){
+let valor = e.target.value;
+// console.log(valor);
+// Adicionar condição
+if(valor.length < 3){
+    usernameInput.classList.add("error");
+    usernameSpans.classList.add("visible");
+    usernameSpans.innerText = "O nome de usuário deve ter no mínimo 3 caracteres";
+    usernameInput.classList.remove("correct");
+}else{
+    usernameInput.classList.remove("error");
+    usernameInput.classList.add("correct");
+    usernameSpans.classList.remove("visible");
 }
-nameInput.addEventListener("change", (e)=>{
-    let valor = e.target.value
-    if(valor.length < 3){
-      spanInput.innerText = "Seu "
-    }
 })
 
-
-
-
-
-
-
-// const required = document.querySelectorAll('.required');
-// console.log(required);
-// const spans = document.querySelectorAll('.span-required');
-// console.log(span-required)
-// const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
-
-// function nameValidate(){
-//     if(campos[0].value.length < 3){
-
-//     } else {
-        
-//     }
-// }
- 
+// Campo obrigatório para o Email
+emailInput.addEventListener("focus", () => {
+    emailLabel.classList.add("required-popup");
+})
+//----------Ocultar popup de campo obrigatório----------//
+emailInput.addEventListener("blur", () => {
+    emailLabel.classList.remove("required-popup");
+})
+//----------Validar valor do input----------//
+emailInput.addEventListener("change", (e) =>{
+let valor = e.target.value;
+// condição
+if(valor.includes("@") && valor.includes(".com")){
+    emailInput.classList.add("correct");
+    emailSpans.classList.remove("visible");
+    emailInput.classList.remove("correct");
+}else{
+    emailInput.classList.add("error");
+    emailSpans.classList.add("visible");
+    emailSpans.innerText = "Insira um e-mail válido";
+}
+})
