abfd3e81c148e2778d12cfd0103bbaccdff99a40
Giovanna D'Alcântara <alcangio@protonmail.com>
2024-05-29 13:58:38 -0300

    .

--- flipbook.js
+++ flipbook.js
@@ -1,11 +1,25 @@
 // abre o flipbook como pop-up
-let bookPopUp = document.getElementsByClassName("book-pop")[0];
-console.log(bookPopUp)
+// let bookPopUp = document.getElementsByClassName("book-pop")[0];
+// console.log(bookPopUp)
 
-bookPopUp.addEventListener("click", (e) => {
-    if (window.location.pathname === 'index.html') {
-        window.open('./public/flipbook.html', 'popup', 'width=1920,height=1080');
-    } else {
-        window.open('./flipbook.html', 'popup', 'width=1920,height=1080');
-    }
-})
+// bookPopUp.addEventListener("click", (e) => {
+//     if (window.location.pathname === 'index.html') {
+//         window.open('./public/flipbook.html', 'popup', 'width=1920,height=1080');
+//     } else {
+//         window.open('./flipbook.html', 'popup', 'width=1920,height=1080');
+//     }
+// })
+document.addEventListener('DOMContentLoaded', () => {
+    let bookPopUp = document.getElementsByClassName("pop-book")[0];
+    console.log(bookPopUp);
+
+    bookPopUp.addEventListener("click", (e) => {
+        e.preventDefault(); // Previne o comportamento padrão do link
+        if (window.location.pathname === '/index.html' || window.location.pathname === '/' || window.location.pathname.endsWith('/index.html')) {
+            window.open('public/flipbook.html', 'popup', 'width=1920,height=1080');
+        } else {
+            window.open('public/flipbook.html', 'popup', 'width=1920,height=1080');
+        }
+    });
+});
+
