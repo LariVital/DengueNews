6a17cb2ec6ea2d737b58452c6d85c00a4249a898
Giovanna D'Alcântara <alcangio@protonmail.com>
2024-05-29 14:08:32 -0300

    retirada condicional do script pop-up de sobre a dengue

--- flipbook.js
+++ flipbook.js
@@ -24,11 +24,21 @@
 // });
 
 document.addEventListener('DOMContentLoaded', () => {
-    let bookPopUp = document.getElementsByClassName("book-pop")[0];
+    let bookPopUp = document.getElementsByClassName("pop-book")[0];
     console.log(bookPopUp);
 
     bookPopUp.addEventListener("click", (e) => {
         e.preventDefault(); // Previne o comportamento padrão do link
         window.open('public/flipbook.html', 'popup', 'width=1920,height=1080');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    let bookPopUp = document.getElementsByClassName("book-pop")[0];
+    console.log(bookPopUp);
+
+    bookPopUp.addEventListener("click", (e) => {
+        e.preventDefault(); // Previne o comportamento padrão do link
+        window.open('flipbook.html', 'popup', 'width=1920,height=1080');
     });
 });
