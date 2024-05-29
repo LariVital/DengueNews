d2155bda39b6c46a505bb71e6defd690ffb656dc
Giovanna D'Alcântara <alcangio@protonmail.com>
2024-05-29 14:04:51 -0300

    teste

--- flipbook.js
+++ flipbook.js
@@ -9,17 +9,26 @@
 //         window.open('./flipbook.html', 'popup', 'width=1920,height=1080');
 //     }
 // })
+// document.addEventListener('DOMContentLoaded', () => {
+//     let bookPopUp = document.getElementsByClassName("pop-book")[0];
+//     console.log(bookPopUp);
+
+//     bookPopUp.addEventListener("click", (e) => {
+//         e.preventDefault(); // Previne o comportamento padrão do link
+//         if (window.location.pathname === '/index.html' || window.location.pathname === '/' || window.location.pathname.endsWith('/index.html')) {
+//             window.open('https://larivital.github.io/DengueNews/public/flipbook.html', 'popup', 'width=1920,height=1080');
+//         } else {
+//             window.open('public/flipbook.html', 'popup', 'width=1920,height=1080');
+//         }
+//     });
+// });
+
 document.addEventListener('DOMContentLoaded', () => {
-    let bookPopUp = document.getElementsByClassName("pop-book")[0];
+    let bookPopUp = document.getElementsByClassName("book-pop")[0];
     console.log(bookPopUp);
 
     bookPopUp.addEventListener("click", (e) => {
         e.preventDefault(); // Previne o comportamento padrão do link
-        if (window.location.pathname === '/index.html' || window.location.pathname === '/' || window.location.pathname.endsWith('/index.html')) {
-            window.open('https://larivital.github.io/DengueNews/public/flipbook.html', 'popup', 'width=1920,height=1080');
-        } else {
-            window.open('public/flipbook.html', 'popup', 'width=1920,height=1080');
-        }
+        window.open('public/flipbook.html', 'popup', 'width=1920,height=1080');
     });
 });
-
